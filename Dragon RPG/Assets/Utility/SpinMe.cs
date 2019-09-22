2a1d16c02f85552bb8814819b1dd8eca2e9a7c26
Filipe Castanheira <castanhf@mail.gvsu.edu>
2019-09-22 17:07:09 -0400

    Enemies can now run to player (nav mesh implemented)

---
+++ Dragon RPG/Assets/Utility/SpinMe.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinMe : MonoBehaviour {
+
+	[SerializeField] float xRotationsPerMinute = 1f;
+	[SerializeField] float yRotationsPerMinute = 1f;
+	[SerializeField] float zRotationsPerMinute = 1f;
+	
+	void Update () {
+
+        // xDegreesPerFrame = Time.deltaTime / 60 * 360 * xRotationsPerMinute
+        // xDegreesPerFrame = seconds frame^-1 / seconds minute^-1 * degrees rotation^-1 * rotation minute^-1
+
+        float xDegreesPerFrame = Time.deltaTime / 60 * 360 * xRotationsPerMinute;
+        // transform.right = x
+        transform.RotateAround (transform.position, transform.right, xDegreesPerFrame);
+
+        float yDegreesPerFrame = Time.deltaTime / 60 * 360 * yRotationsPerMinute;
+        // transform.right = y
+        transform.RotateAround (transform.position, transform.up, yDegreesPerFrame);
+
+        float zDegreesPerFrame = Time.deltaTime / 60 * 360 * zRotationsPerMinute;
+        // transform.right = z
+        transform.RotateAround (transform.position, transform.forward, zDegreesPerFrame);
+	}
+}
